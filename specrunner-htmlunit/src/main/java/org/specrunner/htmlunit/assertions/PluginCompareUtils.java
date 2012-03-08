389a38caf9cfbb32707dd130f29e027415993991
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-03-08 03:46:26 +0000

    Comparator show error if date format is invalid.

--- specrunner-htmlunit/src/main/java/org/specrunner/htmlunit/assertions/PluginCompareUtils.java
+++ specrunner-htmlunit/src/main/java/org/specrunner/htmlunit/assertions/PluginCompareUtils.java
@@ -27,6 +27,7 @@ import org.specrunner.htmlunit.util.WritablePage;
 import org.specrunner.plugins.PluginException;
 import org.specrunner.result.IResultSet;
 import org.specrunner.result.Status;
+import org.specrunner.util.UtilLog;
 import org.specrunner.util.aligner.IStringAligner;
 import org.specrunner.util.aligner.IStringAlignerFactory;
 import org.specrunner.util.aligner.impl.DefaultAlignmentException;
@@ -58,24 +59,41 @@ public final class PluginCompareUtils {
                 result.addResult(Status.FAILURE, block, new DefaultAlignmentException(al), new WritablePage(page));
             }
         } catch (Exception e) {
+            if (UtilLog.LOG.isDebugEnabled()) {
+                UtilLog.LOG.debug(e.getMessage(), e);
+            }
             throw new PluginException(e);
         }
     }
 
     public static boolean compareDate(String format, long tolerance, String expected, String received, IBlock block, IContext context, IResultSet result, SgmlPage page) throws PluginException {
         boolean res = true;
+        DateTimeFormatter fmt = null;
         try {
-            DateTimeFormatter fmt = DateTimeFormat.forPattern(format);
-            DateTime dtExpected = fmt.parseDateTime(expected);
-            DateTime dtReceived = fmt.parseDateTime(received);
-            if (Math.abs(dtExpected.getMillis() - dtReceived.getMillis()) <= tolerance) {
-                result.addResult(Status.SUCCESS, block);
-            } else {
-                addError(expected, received, block, context, result, page);
-                res = false;
-            }
+            fmt = DateTimeFormat.forPattern(format);
         } catch (Exception e) {
-            addError(expected, received, block, context, result, page);
+            if (UtilLog.LOG.isDebugEnabled()) {
+                UtilLog.LOG.debug(e.getMessage(), e);
+            }
+            result.addResult(Status.FAILURE, block, new PluginException(e));
+            res = false;
+        }
+        if (fmt != null) {
+            try {
+                DateTime dtExpected = fmt.parseDateTime(expected);
+                DateTime dtReceived = fmt.parseDateTime(received);
+                if (Math.abs(dtExpected.getMillis() - dtReceived.getMillis()) <= tolerance) {
+                    result.addResult(Status.SUCCESS, block);
+                } else {
+                    addError(expected, received, block, context, result, page);
+                    res = false;
+                }
+            } catch (Exception e) {
+                if (UtilLog.LOG.isDebugEnabled()) {
+                    UtilLog.LOG.debug(e.getMessage(), e);
+                }
+                addError(expected, received, block, context, result, page);
+            }
         }
         return res;
     }
