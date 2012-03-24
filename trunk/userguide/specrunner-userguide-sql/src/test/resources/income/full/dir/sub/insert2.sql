19adb1e78eeefb4344311bf3bb8cae74cbcfdefe
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-03-24 23:07:15 +0000

    Checkstyle adjusts.

--- trunk/userguide/specrunner-userguide-sql/src/test/resources/income/full/dir/sub/insert2.sql
+++ trunk/userguide/specrunner-userguide-sql/src/test/resources/income/full/dir/sub/insert2.sql
@@ -1,3 +1,4 @@
 insert into CTI.CIT_CITY values (2,now(),now(),'Segunda',1)
+insert into CTI.CIT_CITY values (3,now(),now(),'${"Segunda "+" feira"}',${20/20})
 insert into CTI.ADD_ADDRESS values (1,'Rua da primeira cidade',1)
 insert into CTI.ADD_ADDRESS values (2,'Rua da segunda cidade',2)
