9d993840e1105a45462c0e2845aeb620be662cc9
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-03-31 01:06:10 +0000

    Adding copyright information + minor Checkstyle adjusts.

--- userguide/specrunner-userguide-sql/src/test/java/example/sql/DataSourceProviderImpl.java
+++ userguide/specrunner-userguide-sql/src/test/java/example/sql/DataSourceProviderImpl.java
@@ -1,3 +1,20 @@
+/*
+    SpecRunner - Acceptance Test Driven Development Tool
+    Copyright (C) 2011-2012  Thiago Santos
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package example.sql;
 
 import javax.sql.DataSource;
@@ -15,12 +32,14 @@ import org.specrunner.sql.impl.SimpleDataSource;
  */
 public class DataSourceProviderImpl implements IDataSourceProvider {
 
-    private SimpleDataSource source = new SimpleDataSource("org.hsqldb.jdbcDriver", "jdbc:hsqldb:mem:TESTE_PROVIDER", "sa", "");
+    private final SimpleDataSource source = new SimpleDataSource("org.hsqldb.jdbcDriver", "jdbc:hsqldb:mem:TESTE_PROVIDER", "sa", "");
 
+    @Override
     public DataSource getDataSource() {
         return source;
     }
 
+    @Override
     public void release() {
         source.release();
     }
