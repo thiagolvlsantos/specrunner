762e33792d8eb3a1e5b650bce267bdd9965bc629
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-07-29 05:30:35 +0000

    Optimizing HtmlUnit.

---
+++ trunk/specrunner-webdriver/src/main/java/org/specrunner/webdriver/OptimizedCssErrorHandler.java
@@ -0,0 +1,43 @@
+package org.specrunner.webdriver;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.css.sac.CSSParseException;
+
+import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
+
+/**
+ * Default implementation of CSS handler which log only if required.
+ * 
+ * @author Thiago Santos
+ */
+@SuppressWarnings("serial")
+public class OptimizedCssErrorHandler extends DefaultCssErrorHandler {
+
+    /**
+     * Logger, same of DefaultCssErrorHandler.
+     */
+    private static final Log LOG = LogFactory.getLog(DefaultCssErrorHandler.class);
+
+    @Override
+    public void warning(CSSParseException exception) {
+        if (LOG.isWarnEnabled()) {
+            super.warning(exception);
+        }
+    }
+
+    @Override
+    public void error(CSSParseException exception) {
+        if (LOG.isErrorEnabled()) {
+            super.error(exception);
+        }
+    }
+
+    @Override
+    public void fatalError(CSSParseException exception) {
+        if (LOG.isFatalEnabled()) {
+            super.fatalError(exception);
+        }
+    }
+
+}
