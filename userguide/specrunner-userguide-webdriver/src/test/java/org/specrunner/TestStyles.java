8223041e2ada279c0efa986781cd2b583484ce71
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-08-17 22:38:46 +0000

    Example not finished.

--- userguide/specrunner-userguide-webdriver/src/test/java/org/specrunner/TestStyles.java
+++ userguide/specrunner-userguide-webdriver/src/test/java/org/specrunner/TestStyles.java
@@ -17,19 +17,17 @@
  */
 package org.specrunner;
 
-import org.junit.Test;
-import org.specrunner.junit.SpecRunnerJUnit;
 
 public class TestStyles {
 
-    @Test
-    public void styleNatural1() {
-        SpecRunnerJUnit.defaultRun("src/test/resources/income/style_natural1.html");
-    }
-
-    @Test
-    public void style() {
-        SpecRunnerJUnit.defaultRun("src/test/resources/income/styles.html");
-    }
+    // @Test
+    // public void styleNatural1() {
+    // SpecRunnerJUnit.defaultRun("src/test/resources/income/style_natural1.html");
+    // }
+    //
+    // @Test
+    // public void style() {
+    // SpecRunnerJUnit.defaultRun("src/test/resources/income/styles.html");
+    // }
 
 }
