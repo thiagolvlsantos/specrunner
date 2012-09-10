3eb38e6e9e0cfe5cae90e47f08a7b643c467dff6
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-09-10 02:55:36 +0000

    Starting JDBC proxy feature.

--- trunk/specrunner-sql/src/main/java/org/specrunner/sql/proxy/FactoryJdbcBuilder.java
+++ trunk/specrunner-sql/src/main/java/org/specrunner/sql/proxy/FactoryJdbcBuilder.java
@@ -2,10 +2,13 @@ package org.specrunner.sql.proxy;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import javax.sql.DataSource;
 
 /**
  * Wrapped version of a factory.
@@ -15,14 +18,42 @@ import java.sql.Statement;
  */
 public class FactoryJdbcBuilder implements IFactoryJdbc {
 
+    /**
+     * Prefix for wrapper driver.
+     */
+    private static final String JDBC_SR = "jdbc:sr:";
+
+    /**
+     * Unique instance.
+     */
+    protected static FactoryJdbcBuilder instance = new FactoryJdbcBuilder();
+
     static {
         try {
-            FactoryJdbcBuilder factory = new FactoryJdbcBuilder();
-            DriverManager.registerDriver(new DriverWrapper(factory));
+            DriverManager.registerDriver(instance.newDriver(JDBC_SR));
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Get the factory instance.
+     * 
+     * @return The instance.
+     */
+    public static IFactoryJdbc get() {
+        return instance;
+    }
+
+    @Override
+    public Driver newDriver(String prefix) {
+        return new DriverWrapper(this, prefix);
+    }
+
+    @Override
+    public DataSource newDataSource(DataSource ds) {
+        return new DataSourceWrapper(this, ds);
     }
 
     @Override
