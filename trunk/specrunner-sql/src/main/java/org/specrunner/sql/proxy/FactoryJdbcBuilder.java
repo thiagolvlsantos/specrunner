bdfc8d21d1148dd678c18217938b29dabdf75160
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-09-11 03:52:04 +0000

    Bytecode instrumented version.

--- trunk/specrunner-sql/src/main/java/org/specrunner/sql/proxy/FactoryJdbcBuilder.java
+++ trunk/specrunner-sql/src/main/java/org/specrunner/sql/proxy/FactoryJdbcBuilder.java
@@ -8,6 +8,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
 import javax.sql.DataSource;
 
 /**
@@ -16,6 +20,7 @@ import javax.sql.DataSource;
  * @author Thiago Santos
  * 
  */
+@SuppressWarnings("unchecked")
 public class FactoryJdbcBuilder implements IFactoryJdbc {
 
     /**
@@ -28,13 +33,72 @@ public class FactoryJdbcBuilder implements IFactoryJdbc {
      */
     protected static FactoryJdbcBuilder instance = new FactoryJdbcBuilder();
 
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<Driver> driverWrapper;
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<DataSource> dataSourceWrapper;
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<Connection> connectionWrapper;
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<Statement> statementWrapper;
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<PreparedStatement> preparedStatementWrapper;
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<CallableStatement> callableStatementWrapper;
+    /**
+     * The JDBC wrapper class.
+     */
+    protected static Class<ResultSet> resultSetWrapper;
+
     static {
         try {
+            driverWrapper = (Class<Driver>) instrument("org.specrunner.sql.proxy.DriverWrapper");
+            dataSourceWrapper = (Class<DataSource>) instrument("org.specrunner.sql.proxy.DataSourceWrapper");
+            connectionWrapper = (Class<Connection>) instrument("org.specrunner.sql.proxy.ConnectionWrapper");
+            statementWrapper = (Class<Statement>) instrument("org.specrunner.sql.proxy.StatementWrapper");
+            preparedStatementWrapper = (Class<PreparedStatement>) instrument("org.specrunner.sql.proxy.PreparedStatementWrapper");
+            callableStatementWrapper = (Class<CallableStatement>) instrument("org.specrunner.sql.proxy.CallableStatementWrapper");
+            resultSetWrapper = (Class<ResultSet>) instrument("org.specrunner.sql.proxy.ResultSetWrapper");
             DriverManager.registerDriver(instance.newDriver(JDBC_SR));
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Perform instrumentation of classes.
+     * 
+     * @param clazz
+     *            The class name.
+     * @return The modified class.
+     * @throws Exception
+     *             On instrumentation errors.
+     */
+    protected static Class<?> instrument(String clazz) throws Exception {
+        ClassPool cp = ClassPool.getDefault();
+        CtClass cc = cp.get(clazz);
+        for (CtMethod m : cc.getDeclaredMethods()) {
+            CtClass[] types = m.getParameterTypes();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < types.length; i++) {
+                sb.append((i == 0 ? "" : ",") + "%s");
+            }
+            m.insertBefore("System.out.println(\"" + m.getName() + "->\" + String.format(\"" + sb + "\",$args));");
+        }
+        return cc.toClass();
     }
 
     /**
@@ -48,36 +112,64 @@ public class FactoryJdbcBuilder implements IFactoryJdbc {
 
     @Override
     public Driver newDriver(String prefix) {
-        return new DriverWrapper(this, prefix);
+        try {
+            return driverWrapper.getConstructor(IFactoryJdbc.class, String.class).newInstance(this, prefix);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public DataSource newDataSource(DataSource ds) {
-        return new DataSourceWrapper(this, ds);
+        try {
+            return dataSourceWrapper.getConstructor(IFactoryJdbc.class, DataSource.class).newInstance(this, ds);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Connection newConnection(Connection con) {
-        return new ConnectionWrapper(this, con);
+        try {
+            return connectionWrapper.getConstructor(IFactoryJdbc.class, Connection.class).newInstance(this, con);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Statement newStatement(Statement stmt) {
-        return new StatementWrapper(this, stmt);
+        try {
+            return statementWrapper.getConstructor(IFactoryJdbc.class, Statement.class).newInstance(this, stmt);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public PreparedStatement newPreparedStatement(PreparedStatement pstmt) {
-        return new PreparedStatementWrapper(this, pstmt);
+        try {
+            return preparedStatementWrapper.getConstructor(IFactoryJdbc.class, PreparedStatement.class).newInstance(this, pstmt);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public CallableStatement newCallableStatement(CallableStatement cstmt) {
-        return new CallableStatementWrapper(this, cstmt);
+        try {
+            return callableStatementWrapper.getConstructor(IFactoryJdbc.class, CallableStatement.class).newInstance(this, cstmt);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ResultSet newResultSet(ResultSet rs) {
-        return new ResultSetWrapper(this, rs);
+        try {
+            return resultSetWrapper.getConstructor(IFactoryJdbc.class, ResultSet.class).newInstance(this, rs);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
