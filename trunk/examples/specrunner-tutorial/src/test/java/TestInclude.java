dca83c4e47627c1f7c4a1544af85968d2c9c680a
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2012-11-09 02:24:46 +0000

    Library versions upgrade.

--- trunk/examples/specrunner-tutorial/src/test/java/TestInclude.java
+++ trunk/examples/specrunner-tutorial/src/test/java/TestInclude.java
@@ -4,6 +4,8 @@ import org.specrunner.configuration.IConfiguration;
 import org.specrunner.configuration.IConfigurationFactory;
 import org.specrunner.junit.SpecRunnerJUnit;
 import org.specrunner.plugins.impl.include.PluginInclude;
+import org.specrunner.source.ISource;
+import org.specrunner.source.ISourceFactory;
 
 public class TestInclude {
 
@@ -12,5 +14,19 @@ public class TestInclude {
         IConfiguration cfg = SpecRunnerServices.get(IConfigurationFactory.class).newConfiguration();
         cfg.add(PluginInclude.FEATURE_EXPANDED, Boolean.TRUE);
         SpecRunnerJUnit.defaultRun("src/test/resources/income/include.html", cfg);
+    }
+
+    public static void main(String[] args) {
+        try {
+            ISource s;
+            s = SpecRunnerServices.get(ISourceFactory.class).newSource("src/test/resources/income/include.html");
+            System.out.println(s.getDocument().toXML());
+            s = SpecRunnerServices.get(ISourceFactory.class).newSource("src/test/resources/income/dir/destino.html");
+            System.out.println(s.getDocument().toXML());
+            s = SpecRunnerServices.get(ISourceFactory.class).newSource("src/test/resources/income/dir/sub/interno.html");
+            System.out.println(s.getDocument().toXML());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
