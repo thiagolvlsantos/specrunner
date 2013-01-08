5a0a129bf16a1c5cb3f037164c52684a46765816
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-01-08 03:56:14 +0000

    Duplication removal.

--- trunk/examples/specrunner-hibernate3-example/src/test/java/th/example/ConverterDatePattern.java
+++ trunk/examples/specrunner-hibernate3-example/src/test/java/th/example/ConverterDatePattern.java
@@ -6,7 +6,9 @@ import java.text.SimpleDateFormat;
 import org.specrunner.util.converter.ConverterException;
 import org.specrunner.util.converter.IConverter;
 
+@SuppressWarnings("serial")
 public class ConverterDatePattern implements IConverter {
+
     @Override
     public void initialize() {
     }
