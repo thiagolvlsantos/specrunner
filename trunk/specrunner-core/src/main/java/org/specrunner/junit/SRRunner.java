097feddec17b33c957a4a8f56cb290ab2cacae3c
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-04-26 14:40:26 +0000

    Output directory controled by 'sr.output'.

--- trunk/specrunner-core/src/main/java/org/specrunner/junit/SRRunner.java
+++ trunk/specrunner-core/src/main/java/org/specrunner/junit/SRRunner.java
@@ -13,6 +13,7 @@ import org.junit.runners.model.Statement;
 import org.specrunner.SpecRunnerServices;
 import org.specrunner.configuration.IConfiguration;
 import org.specrunner.configuration.IConfigurationFactory;
+import org.specrunner.dumper.impl.AbstractSourceDumperFile;
 import org.specrunner.plugins.impl.elements.PluginHtml;
 import org.specrunner.result.IResultSet;
 
@@ -33,6 +34,11 @@ public class SRRunner extends BlockJUnit4ClassRunner {
      * Allowed extensions of file.
      */
     public static final String EXTENSIONS = System.getProperty("sr.extensions", ".xhtml,.html,.htm");
+
+    /**
+     * Output path.
+     */
+    public static final String PATH = System.getProperty("sr.output", "target/output/");
 
     /**
      * Basic constructor.
@@ -73,7 +79,12 @@ public class SRRunner extends BlockJUnit4ClassRunner {
                 public void evaluate() throws Throwable {
                     IConfiguration cfg = SpecRunnerServices.get(IConfigurationFactory.class).newConfiguration();
                     cfg.add(PluginHtml.BEAN_NAME, test);
-                    IResultSet result = SpecRunnerServices.getSpecRunner().run(getFile(), cfg);
+                    Class<?> clazz = getTestClass().getJavaClass();
+                    File input = getFile(clazz);
+                    File output = getOutput(clazz, input);
+                    cfg.add(AbstractSourceDumperFile.FEATURE_OUTPUT_DIRECTORY, output.getParentFile());
+                    cfg.add(AbstractSourceDumperFile.FEATURE_OUTPUT_NAME, output.getName());
+                    IResultSet result = SpecRunnerServices.getSpecRunner().run(input.getPath(), cfg);
                     if (result.getStatus().isError()) {
                         throw new Exception(result.asString());
                     }
@@ -85,20 +96,36 @@ public class SRRunner extends BlockJUnit4ClassRunner {
     /**
      * Get the HTML file corresponding to the Java.
      * 
-     * @return The HTML file.
+     * @param clazz
+     *            The test class.
+     * 
+     * @return The input HTML file.
      */
-    protected String getFile() {
-        Class<?> clazz = getTestClass().getJavaClass();
+    protected File getFile(Class<?> clazz) {
         URL location = clazz.getProtectionDomain().getCodeSource().getLocation();
         String str = location.toString();
         str = str.replace("file:\\\\", "").replace("file:///", "").replace("file:\\", "").replace("file:/", "");
         String prefix = str + clazz.getPackage().getName().replace(".", File.separator) + File.separator + clazz.getSimpleName();
         String[] extensions = EXTENSIONS.split(",");
         for (String s : extensions) {
-            if (new File(prefix + s).exists()) {
-                return prefix + s;
+            File tmp = new File(prefix + s);
+            if (tmp.exists()) {
+                return tmp;
             }
         }
         throw new RuntimeException("File with one of extensions '" + extensions + "' to " + prefix + " not found!");
+    }
+
+    /**
+     * Get the output file.
+     * 
+     * @param clazz
+     *            The test class.
+     * @param input
+     *            The input file.
+     * @return The output file.
+     */
+    public File getOutput(Class<?> clazz, File input) {
+        return new File(new File(PATH + clazz.getPackage().getName().replace('.', File.separatorChar)).getAbsoluteFile(), input.getName());
     }
 }
