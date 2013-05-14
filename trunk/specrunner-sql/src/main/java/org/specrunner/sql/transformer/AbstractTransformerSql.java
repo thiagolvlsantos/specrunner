41a32cd1594e7d552a514b05c76400310f8f6139
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-05-14 01:29:07 +0000

    Javadoc + listeners adjusts.

--- trunk/specrunner-sql/src/main/java/org/specrunner/sql/transformer/AbstractTransformerSql.java
+++ trunk/specrunner-sql/src/main/java/org/specrunner/sql/transformer/AbstractTransformerSql.java
@@ -1,3 +1,20 @@
+/*
+    SpecRunner - Acceptance Test Driven Development Tool
+    Copyright (C) 2011-2013  Thiago Santos
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package org.specrunner.sql.transformer;
 
 import nu.xom.Attribute;
@@ -9,6 +26,13 @@ import org.specrunner.source.ISource;
 import org.specrunner.source.SourceException;
 import org.specrunner.transformer.ITransformer;
 
+/**
+ * SQL transformer.
+ * 
+ * @author Thiago Santos
+ * 
+ */
+@SuppressWarnings("serial")
 public abstract class AbstractTransformerSql implements ITransformer {
 
     @Override
@@ -26,5 +50,10 @@ public abstract class AbstractTransformerSql implements ITransformer {
         return source;
     }
 
+    /**
+     * Get the tables style.
+     * 
+     * @return The class.
+     */
     public abstract String getValue();
 }
