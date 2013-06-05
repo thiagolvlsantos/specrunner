769e5e0d31ad30dfd07923b1784406a303fa74f4
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-06-05 03:16:28 +0000

    Plugin verify rows fully functional.

--- trunk/specrunner-core/src/main/java/org/specrunner/plugins/impl/logical/PluginVerifyRows.java
+++ trunk/specrunner-core/src/main/java/org/specrunner/plugins/impl/logical/PluginVerifyRows.java
@@ -1,12 +1,13 @@
 package org.specrunner.plugins.impl.logical;
 
-import java.util.List;
+import java.util.Iterator;
 
 import nu.xom.Attribute;
 import nu.xom.Element;
 import nu.xom.Node;
 import nu.xom.Nodes;
 
+import org.specrunner.context.IBlock;
 import org.specrunner.context.IContext;
 import org.specrunner.expressions.Unsilent;
 import org.specrunner.plugins.ActionType;
@@ -16,6 +17,7 @@ import org.specrunner.plugins.impl.AbstractPluginValue;
 import org.specrunner.plugins.impl.UtilPlugin;
 import org.specrunner.plugins.type.Assertion;
 import org.specrunner.result.IResultSet;
+import org.specrunner.result.status.Failure;
 import org.specrunner.util.UtilEvaluator;
 import org.specrunner.util.xom.UtilNode;
 
@@ -65,20 +67,29 @@ public class PluginVerifyRows extends AbstractPluginValue {
     @Override
     public ENext doStart(IContext context, IResultSet result) throws PluginException {
         Node node = context.getNode();
+        IBlock block = context.peek();
         Object value = getValue();
-        if (value == null || !(value instanceof List)) {
-            throw new PluginException("Value not found or invalid type (should be List).");
+        if (value == null || !(value instanceof Iterable)) {
+            throw new PluginException("Value is null ou not found, or value has an invalid type (should be an Iterable).");
         }
-        List<?> l = (List<?>) value;
+        Iterator<?> ite = ((Iterable<?>) value).iterator();
         Nodes ns = node.query("descendant::tr");
         Element head = (Element) ns.get(0);
         Nodes hs = head.query("descendant::th");
+        if (hs.size() == 0) {
+            throw new PluginException("Missing header information.");
+        }
         UtilNode.setIgnore(head);
         String pos = UtilEvaluator.asVariable("index");
         String item = UtilEvaluator.asVariable(var);
-        for (int i = 1; i < ns.size(); i++) {
+        int i = 1;
+        boolean first = true;
+        for (; i < ns.size(); i++) {
             Element row = (Element) ns.get(i);
             Nodes cs = row.query("descendant::td");
+            if (hs.size() != cs.size()) {
+                throw new PluginException("Number of headers (" + hs.size() + ") is different of columns (" + cs.size() + ").");
+            }
             for (int j = 0; j < cs.size(); j++) {
                 Element h = (Element) hs.get(j);
                 Element c = (Element) cs.get(j);
@@ -89,14 +100,47 @@ public class PluginVerifyRows extends AbstractPluginValue {
             }
             context.push(context.newBlock(row, this));
             try {
-                context.saveLocal(pos, "" + (i - 1));
-                context.saveLocal(item, l.get(i - 1));
-                UtilPlugin.performChildren(row, context, result);
+                context.saveLocal(pos, String.valueOf(i - 1));
+                if (ite.hasNext()) {
+                    context.saveLocal(item, ite.next());
+                    UtilPlugin.performChildren(row, context, result);
+                } else {
+                    if (first) {
+                        result.addResult(Failure.INSTANCE, block, new PluginException("Returned enumeration missing itens."));
+                        first = false;
+                    }
+                }
             } finally {
-                context.clearLocal(pos);
                 context.clearLocal(item);
+                context.clearLocal(pos);
                 context.pop();
             }
+        }
+        Element ele = (Element) node;
+        first = true;
+        while (ite.hasNext()) {
+            if (first) {
+                result.addResult(Failure.INSTANCE, context.peek(), new PluginException("Returned enumeration has more elements than expected."));
+                first = false;
+            }
+            Element tr = new Element("tr");
+            for (int j = 0; j < hs.size(); j++) {
+                Element td = new Element("td");
+                td.appendChild("${" + ((Element) hs.get(j)).getAttributeValue("value") + "}");
+                tr.appendChild(td);
+            }
+            ele.appendChild(tr);
+            try {
+                context.push(context.newBlock(tr, this));
+                context.saveLocal(pos, String.valueOf(i - 1));
+                context.saveLocal(item, ite.next());
+                UtilPlugin.performChildren(tr, context, result);
+            } finally {
+                context.clearLocal(item);
+                context.clearLocal(pos);
+                context.pop();
+            }
+            i++;
         }
         return ENext.SKIP;
     }
