2bcebcf40141e9e9b61999be859445af844be869
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-06-05 03:16:28 +0000

    Plugin verify rows fully functional.

--- userguide/specrunner-userguide-sql/src/test/java/example/sql/TestSRSpring.java
+++ userguide/specrunner-userguide-sql/src/test/java/example/sql/TestSRSpring.java
@@ -11,7 +11,15 @@ import org.springframework.test.context.TestExecutionListeners;
 @TestExecutionListeners(inheritListeners = false, listeners = { PrintListener.class })
 public class TestSRSpring {
 
-    public List<Qualquer> lista() {
+    public List<Qualquer> lista1() {
         return Arrays.asList(new Qualquer(1, "Eu"), new Qualquer(2, "Tu"));
+    }
+
+    public List<Qualquer> lista2() {
+        return Arrays.asList(new Qualquer(1, "Eu"));
+    }
+
+    public List<Qualquer> lista3() {
+        return Arrays.asList(new Qualquer(1, "Eu"), new Qualquer(2, "Tu"), new Qualquer(3, "Ele"));
     }
 }
