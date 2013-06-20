f766d65c93c45c4be26b14565bdc75c278646575
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-06-20 03:17:31 +0000

    Test adjusts.

--- userguide/specrunner-userguide-sql/src/test/java/example/sql/Qualquer.java
+++ userguide/specrunner-userguide-sql/src/test/java/example/sql/Qualquer.java
@@ -3,14 +3,16 @@ package example.sql;
 public class Qualquer {
     public int numero;
     public String nome;
+    public String sobrenome;
 
-    public Qualquer(int numero, String nome) {
+    public Qualquer(int numero, String nome, String sobrenome) {
         this.numero = numero;
         this.nome = nome;
+        this.sobrenome = sobrenome;
     }
 
     @Override
     public String toString() {
-        return "Qualquer [numero=" + numero + ", nome=" + nome + "]";
+        return "Qualquer [numero=" + numero + ", nome=" + nome + ", sobrenome=" + sobrenome + "]";
     }
 }
