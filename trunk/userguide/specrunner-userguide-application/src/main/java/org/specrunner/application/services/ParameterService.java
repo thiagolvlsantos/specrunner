98b06b7bbb845bbba48fa5964d3d2fb7de35af75
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-08-12 05:45:29 +0000

    Crude implemented.

--- trunk/userguide/specrunner-userguide-application/src/main/java/org/specrunner/application/services/ParameterService.java
+++ trunk/userguide/specrunner-userguide-application/src/main/java/org/specrunner/application/services/ParameterService.java
@@ -1,13 +1,16 @@
 package org.specrunner.application.services;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,23 +22,20 @@ import org.specrunner.application.entities.Parameter;
 import org.specrunner.application.entities.ParameterType;
 
 @Path("/parameter")
+@Consumes("application/json")
+@Produces("application/json")
 public class ParameterService {
 
     private static Map<Long, Parameter> params = new HashMap<Long, Parameter>();
-
     static {
         Parameter p1 = new Parameter(ParameterType.BOOLEAN, Boolean.TRUE);
         p1.setId(1L);
-
         Parameter p2 = new Parameter(ParameterType.DOUBLE, 10.5);
         p2.setId(2L);
-
         Parameter p3 = new Parameter(ParameterType.LONG, 35L);
         p3.setId(3L);
-
         Parameter p4 = new Parameter(ParameterType.STRING, "working!");
         p4.setId(4L);
-
         params.put(p1.getId(), p1);
         params.put(p2.getId(), p2);
         params.put(p3.getId(), p3);
@@ -44,30 +44,50 @@ public class ParameterService {
 
     @GET
     @Path("/all")
-    @Produces("application/json")
-    public Collection<Parameter> get() {
+    public Collection<Parameter> all() {
         return params.values();
     }
 
     @GET
-    @Path("/get/{id}")
-    @Produces("application/json")
-    public Parameter get(@PathParam("id") Long id) {
-        return params.get(id);
+    @Path("/select/{id}")
+    public Response select(@PathParam("id") Long id) {
+        Parameter tmp = params.get(id);
+        if (tmp == null) {
+            return Response.status(Status.NOT_FOUND).entity("Not found.").build();
+        }
+        return Response.ok(tmp).build();
     }
 
     @POST
-    @Path("/set/{id}")
-    @Consumes("application/json")
-    public Response set(@PathParam("id") Long id, Parameter parameter) {
-        Parameter p = params.get(id);
+    @Path("/insert")
+    public Response insert(Parameter parameter) {
+        Set<Long> keys = params.keySet();
+        Long max = keys.isEmpty() ? 0 : Collections.max(keys);
+        parameter.setId(max + 1);
+        params.put(parameter.getId(), parameter);
+        return Response.ok(parameter).status(Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("/update")
+    public Response update(Parameter parameter) {
+        Parameter tmp = params.get(parameter.getId());
         try {
-            BeanUtils.copyProperties(p, parameter);
-        } catch (IllegalAccessException e) {
-            return Response.status(Status.BAD_REQUEST).build();
-        } catch (InvocationTargetException e) {
+            BeanUtils.copyProperties(tmp, parameter);
+        } catch (Exception e) {
             return Response.status(Status.BAD_REQUEST).build();
         }
-        return Response.ok(p).build();
+        return Response.ok(tmp).build();
+    }
+
+    @DELETE
+    @Path("/delete/{id}")
+    public Response delete(@PathParam("id") Long id) {
+        Parameter old = params.get(id);
+        if (old == null) {
+            return Response.status(Status.NOT_FOUND).entity("Not removed.").build();
+        }
+        params.remove(id);
+        return Response.ok(old).build();
     }
 }
