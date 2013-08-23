e9156ee24562121b083c315e09eced2db873a235
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-08-23 04:03:14 +0000

    Natural language support extended.

---
+++ trunk/userguide/specrunner-userguide-core/src/test/java/example/language/TestPhrase.java
@@ -0,0 +1,55 @@
+package example.language;
+
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.specrunner.SpecRunnerServices;
+import org.specrunner.expressions.IExpressionFactory;
+import org.specrunner.junit.ExpectedMessages;
+import org.specrunner.junit.SRRunner;
+import org.specrunner.plugins.impl.language.Converter;
+import org.specrunner.util.converter.impl.ConverterDatePatternArgs;
+
+@RunWith(SRRunner.class)
+public class TestPhrase {
+
+    @Before
+    public void before() {
+        SpecRunnerServices.get(IExpressionFactory.class).bindClass("dt", DateTime.class);
+    }
+
+    @ExpectedMessages(messages = { "Falhou!" })
+    public void openFileInBrowser(String file) throws Exception {
+        String tmp = System.getProperty("user.home");
+        if (!tmp.equals(file)) {
+            throw new Exception(tmp + " != " + file);
+        }
+        throw new Exception("Falhou!");
+    }
+
+    public void openFileInBrowserString(String file, String other) throws Exception {
+        System.out.println("CALLED.1:" + file + "," + other);
+    }
+
+    public void openFileInBrowserInteger(String file, Integer other) throws Exception {
+        System.out.println("CALLED.2:" + file + "," + other);
+    }
+
+    public void openFileInBrowserFloat(String file, Float other) throws Exception {
+        System.out.println("CALLED.3:" + file + "," + other);
+    }
+
+    public void openFileInBrowserObject(String file, Object other) throws Exception {
+        System.out.println("CALLED.4:" + file + "," + other);
+    }
+
+    public void openFileInBrowserDate(String file, @Converter(name = "datePatternArgs", args = { "dd/MM/yyyy" }) Date other) throws Exception {
+        System.out.println("CALLED.5:" + file + "," + other);
+    }
+
+    public void openFileInBrowserDateTyped(String file, @Converter(type = ConverterDatePatternArgs.class, args = { "dd/MM/yyyy" }) Date other) throws Exception {
+        System.out.println("CALLED.6:" + file + "," + other);
+    }
+}
---
+++ trunk/userguide/specrunner-userguide-core/src/test/java/example/language/TestSentence.java
@@ -0,0 +1,34 @@
+package example.language;
+
+import org.junit.runner.RunWith;
+import org.specrunner.junit.ExpectedMessages;
+import org.specrunner.junit.SRRunner;
+
+@RunWith(SRRunner.class)
+public class TestSentence {
+
+    @ExpectedMessages(messages = { "Falhou!" })
+    public void openFileInBrowser(String file) throws Exception {
+        String tmp = System.getProperty("user.home");
+        if (!tmp.equals(file)) {
+            throw new Exception(tmp + " != " + file);
+        }
+        throw new Exception("Falhou!");
+    }
+
+    public void openFileInBrowser(String file, String other) throws Exception {
+        System.out.println("CALLED.1:" + file + "," + other);
+    }
+
+    public void openFileInBrowser(String file, Integer other) throws Exception {
+        System.out.println("CALLED.2:" + file + "," + other);
+    }
+
+    public void openFileInBrowser(String file, Float other) throws Exception {
+        System.out.println("CALLED.3:" + file + "," + other);
+    }
+
+    public void openFileInBrowser(String file, Object other) throws Exception {
+        System.out.println("CALLED.4:" + file + "," + other);
+    }
+}
