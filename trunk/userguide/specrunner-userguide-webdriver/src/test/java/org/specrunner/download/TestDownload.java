fb0ef588589e7ca16303b1ae046fd10f47379a51
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-08-27 03:07:49 +0000

    Download binary prototype.

--- trunk/userguide/specrunner-userguide-webdriver/src/test/java/org/specrunner/download/TestDownload.java
+++ trunk/userguide/specrunner-userguide-webdriver/src/test/java/org/specrunner/download/TestDownload.java
@@ -2,16 +2,14 @@ package org.specrunner.download;
 
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.specrunner.SpecRunnerServices;
 import org.specrunner.junit.SRRunner;
-import org.specrunner.webdriver.PluginBrowser;
-import org.specrunner.webdriver.impl.WebDriverFactoryChrome;
 
 @RunWith(SRRunner.class)
 public class TestDownload {
 
     @BeforeClass
     public static void antes() {
-        SpecRunnerServices.getFeatureManager().add(PluginBrowser.FEATURE_WEBDRIVER_FACTORY, WebDriverFactoryChrome.class.getName());
+        // SpecRunnerServices.getFeatureManager().add(PluginBrowser.FEATURE_WEBDRIVER_FACTORY,
+        // WebDriverFactoryChrome.class.getName());
     }
 }
