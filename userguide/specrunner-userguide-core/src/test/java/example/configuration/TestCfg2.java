8aab8b2213152ea1cd1b58ad60a202e007e95a99
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-09-14 00:51:18 +0000

    Better configuration example.

--- userguide/specrunner-userguide-core/src/test/java/example/configuration/TestCfg2.java
+++ userguide/specrunner-userguide-core/src/test/java/example/configuration/TestCfg2.java
@@ -2,19 +2,24 @@ package example.configuration;
 
 import org.junit.runner.RunWith;
 import org.specrunner.SpecRunnerServices;
+import org.specrunner.configuration.IConfiguration;
+import org.specrunner.junit.Configuration;
 import org.specrunner.junit.ExpectedMessage;
 import org.specrunner.junit.SRRunner;
 import org.specrunner.plugins.impl.var.PluginBean;
 
 @RunWith(SRRunner.class)
-public class TestCfg2 {
+public class TestCfg2 extends TestCfg1 {
 
+    @Override
+    @Configuration
+    public void prepare(IConfiguration cfg) {
+        SpecRunnerServices.getFeatureManager().add(PluginBean.FEATURE_WAIT, "wait");
+    }
+
+    @Override
     @ExpectedMessage("Expected result of 'public boolean example.configuration.TestCfg2.checkCfg(java.lang.String)' must be 'true'. Received 'false'.")
     public boolean checkCfg(String value) {
         return value.equals(SpecRunnerServices.getFeatureManager().get(PluginBean.BEAN_NAME));
-    }
-
-    public boolean checkAll(String value) {
-        return value.equals(SpecRunnerServices.getFeatureManager().get(PluginBean.FEATURE_WAIT));
     }
 }
