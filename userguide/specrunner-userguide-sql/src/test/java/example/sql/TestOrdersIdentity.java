d2e8667e9dc2f605531eeafa4b103afd4a8bc3c7
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-10-25 13:52:38 +0000

    Test refactorign.

--- userguide/specrunner-userguide-sql/src/test/java/example/sql/TestOrdersIdentity.java
+++ userguide/specrunner-userguide-sql/src/test/java/example/sql/TestOrdersIdentity.java
@@ -1,56 +1,24 @@
 package example.sql;
 
-import org.joda.time.LocalDate;
-import org.junit.Before;
 import org.junit.Test;
-import org.specrunner.SpecRunnerServices;
-import org.specrunner.comparators.impl.ComparatorDate;
-import org.specrunner.configuration.IConfiguration;
-import org.specrunner.configuration.IConfigurationFactory;
-import org.specrunner.expressions.IExpressionFactory;
-import org.specrunner.junit.SpecRunnerJUnit;
-import org.specrunner.sql.IDatabase;
-import org.specrunner.sql.PluginConnection;
-import org.specrunner.sql.PluginDatabase;
 import org.specrunner.sql.PluginSchema;
-import org.specrunner.sql.PluginSchemaLoader;
-import org.specrunner.sql.impl.Database;
-import org.specrunner.sql.meta.impl.SchemaLoaderXOM;
 
-public class TestOrdersIdentity {
-
-    private static final String INCOME = "src/test/resources/income/orders/";
-    private static final String OUTCOME = "src/test/resources/outcome/orders/";
-
-    protected IConfiguration cfg;
+public class TestOrdersIdentity extends AbstractTestOrders {
 
     public TestOrdersIdentity() {
-    }
-
-    @Before
-    public void before() {
-        cfg = SpecRunnerServices.get(IConfigurationFactory.class).newConfiguration();
-        cfg.add(PluginConnection.FEATURE_PROVIDER_INSTANCE, new DataSourceProviderImpl());
-        cfg.add(PluginConnection.FEATURE_REUSE, true);
-        cfg.add(PluginSchemaLoader.FEATURE_PROVIDER_INSTANCE, new SchemaLoaderXOM());
-        cfg.add(PluginSchemaLoader.FEATURE_REUSE, true);
+        super();
         cfg.add(PluginSchema.FEATURE_SOURCE, "/income/orders/orders.cfg.xml");
         cfg.add(PluginSchema.FEATURE_REUSE, true);
-        cfg.add(PluginDatabase.FEATURE_PROVIDER_INSTANCE, new IDatabase[] { new Database() });
-        cfg.add(PluginDatabase.FEATURE_REUSE, true);
-        // time comparators tolerance of 1000 milliseconds.
-        cfg.add(ComparatorDate.FEATURE_TOLERANCE, 1000L);
-
-        // expressions
-        SpecRunnerServices.get(IExpressionFactory.class).bindValue("d", new LocalDate().toString("MM/dd/yyyy"));
     }
 
-    protected void run(String name) {
-        run(name, name);
+    @Override
+    public String getIncome() {
+        return "src/test/resources/income/orders/";
     }
 
-    protected void run(String name, String out) {
-        SpecRunnerJUnit.defaultRun(INCOME + name, OUTCOME + out, cfg);
+    @Override
+    public String getOutcome() {
+        return "src/test/resources/outcome/orders/";
     }
 
     @Test
