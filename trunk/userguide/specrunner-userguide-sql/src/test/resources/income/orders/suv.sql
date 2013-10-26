ac90e2355ffe876f6c8dd8cf9f87e509efbda18d
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-10-26 13:59:00 +0000

    Removal or update in blocks. Use 'count=?' to the D ou U command line to sign the expected number of registers deleted or updated.

--- trunk/userguide/specrunner-userguide-sql/src/test/resources/income/orders/suv.sql
+++ trunk/userguide/specrunner-userguide-sql/src/test/resources/income/orders/suv.sql
@@ -5,3 +5,4 @@ insert into ORD.line_items (line_number,order_id,product_id,quantity) values (2,
 insert into ORD.line_items (line_number,order_id,product_id,quantity) values (3,2,2,10)
 delete from ORD.line_items where line_number=3 and order_id=2
 insert into ORD.line_items (line_number,order_id,product_id,quantity) values (3,2,3,10)
+delete from ORD.customers where name = 'Ringo Star'
