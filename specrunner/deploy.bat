9a1d19dda88ccbb0eebd73a67ed5a85293bab35c
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-10-28 01:53:19 +0000

    Updated version.

--- specrunner/deploy.bat
+++ specrunner/deploy.bat
@@ -1,2 +1,2 @@
 @rem mvn clean package verify release:perform -Darguments=-Dgpg.passphrase=%1
-cls & mvn -Pdeploy clean source:jar javadoc:jar deploy
+cls & mvn -U -Pdeploy clean source:jar javadoc:jar deploy
