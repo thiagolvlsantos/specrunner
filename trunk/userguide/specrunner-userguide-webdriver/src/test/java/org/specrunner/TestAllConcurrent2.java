2bf44e09ba66e26830243a1822dbdccfee7dcceb
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-11-09 16:46:15 +0000

    Split examples.

--- trunk/userguide/specrunner-userguide-webdriver/src/test/java/org/specrunner/TestAllConcurrent2.java
+++ trunk/userguide/specrunner-userguide-webdriver/src/test/java/org/specrunner/TestAllConcurrent2.java
@@ -1,8 +1,10 @@
 package org.specrunner;
 
 import org.junit.runner.RunWith;
+import org.specrunner.junit.Concurrent;
 import org.specrunner.junit.ConcurrentRunner;
 
 @RunWith(ConcurrentRunner.class)
-public class TestAllConcurrent extends TestAllSequential {
+@Concurrent(threads = 2)
+public class TestAllConcurrent2 extends TestAllSequential {
 }
