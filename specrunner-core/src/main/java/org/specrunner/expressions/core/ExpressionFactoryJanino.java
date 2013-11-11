fb910452257a9bfbaa52e2c5c0387a943d62cb76
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2013-11-11 04:54:22 +0000

    Packages refactoring.

--- specrunner-core/src/main/java/org/specrunner/expressions/core/ExpressionFactoryJanino.java
+++ specrunner-core/src/main/java/org/specrunner/expressions/core/ExpressionFactoryJanino.java
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-package org.specrunner.expressions.impl;
+package org.specrunner.expressions.core;
 
 import org.specrunner.context.IContext;
 import org.specrunner.expressions.ExpressionException;
