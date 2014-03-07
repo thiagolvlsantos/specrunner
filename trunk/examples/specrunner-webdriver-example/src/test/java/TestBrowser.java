8e79615ba2510a7d1166e714386918d9ac91f5b5
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-03-07 02:12:01 +0000

    Revision upgrade.

--- trunk/examples/specrunner-webdriver-example/src/test/java/TestBrowser.java
+++ trunk/examples/specrunner-webdriver-example/src/test/java/TestBrowser.java
@@ -1,5 +1,4 @@
 import org.junit.Test;
-import org.openqa.selenium.android.AndroidDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.specrunner.SRServices;
 import org.specrunner.configuration.IConfiguration;
