a3015edcaadf3c64d46bc3c856e3c6c7f5d8ed01
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-04-03 05:56:36 +0000

    version 1.3.17

---
+++ tags/specrunner-1.3.17/specrunner-core/src/main/java/org/specrunner/converters/UtilConverter.java
@@ -0,0 +1,191 @@
+/*
+    SpecRunner - Acceptance Test Driven Development Tool
+    Copyright (C) 2011-2013  Thiago Santos
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package org.specrunner.converters;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.specrunner.SRServices;
+import org.specrunner.plugins.PluginException;
+import org.specrunner.plugins.core.objects.PluginObjectManager;
+import org.specrunner.util.UtilLog;
+
+/**
+ * Converter utilities.
+ * 
+ * @author Thiago Santos
+ * 
+ */
+public final class UtilConverter {
+
+    /**
+     * Map of primitive types for correct use of
+     * <code>&lt;type&gt;.isInstance(...)</code>.
+     */
+    public static final Map<Class<?>, Class<?>> PRIMITIVES = new HashMap<Class<?>, Class<?>>();
+
+    /**
+     * Default constructor.
+     */
+    private UtilConverter() {
+    }
+
+    static {
+        PRIMITIVES.put(boolean.class, Boolean.class);
+        PRIMITIVES.put(char.class, Character.class);
+        PRIMITIVES.put(byte.class, Byte.class);
+        PRIMITIVES.put(short.class, Short.class);
+        PRIMITIVES.put(int.class, Integer.class);
+        PRIMITIVES.put(long.class, Long.class);
+        PRIMITIVES.put(float.class, Float.class);
+        PRIMITIVES.put(double.class, Double.class);
+    }
+
+    /**
+     * Get errors.
+     * 
+     * @param clazz
+     *            O errors.
+     * @return The class.
+     */
+    public static Class<?> getWrapper(Class<?> clazz) {
+        Class<?> primitive = PRIMITIVES.get(clazz);
+        return primitive == null ? clazz : primitive;
+    }
+
+    /**
+     * Prepare arguments.
+     * 
+     * @param method
+     *            The method.
+     * @param arguments
+     *            The arguments.
+     * @throws PluginException
+     *             On preparation errors.
+     */
+    public static void prepareMethodArguments(Method method, List<Object> arguments) throws PluginException {
+        Class<?>[] types = method.getParameterTypes();
+        if (types.length != arguments.size()) {
+            throw new PluginException(method + " has different number of arguments. Received arguments:" + arguments);
+        }
+        Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+        for (int i = 0; i < types.length; i++) {
+            arguments.add(i, prepareArgument(method.toString(), parameterAnnotations[i], types[i], arguments.get(i)));
+            arguments.remove(i + 1);
+        }
+    }
+
+    /**
+     * Prepare only one argument.
+     * 
+     * @param message
+     *            The message.
+     * @param annotations
+     *            A list of annotations.
+     * @param type
+     *            The object expected type..
+     * @param arg
+     *            The current argument.
+     * @return The result of argument preparation.
+     * @throws PluginException
+     *             On preparation errors.
+     */
+    public static Object prepareArgument(String message, Annotation[] annotations, Class<?> type, Object arg) throws PluginException {
+        Class<?> tmpType = PRIMITIVES.get(type);
+        if (tmpType != null) {
+            type = tmpType;
+        }
+        if (!type.isInstance(arg)) {
+            String simpleName = type.getSimpleName().toLowerCase();
+            IConverter converter = null;
+            Object[] converterArguments = null;
+            Converter annotation = getConverter(annotations);
+            IConverterManager cm = SRServices.getConverterManager();
+            if (annotation != null) {
+                String name = annotation.name();
+                if (!name.isEmpty()) {
+                    converter = cm.get(name);
+                    if (converter == null) {
+                        throw new PluginException("Converter named '" + name + "' not found.");
+                    }
+                } else {
+                    Class<? extends IConverter> converterType = annotation.type();
+                    if (converterType != IConverter.class) {
+                        try {
+                            converter = converterType.newInstance();
+                        } catch (InstantiationException e) {
+                            throw new PluginException(e);
+                        } catch (IllegalAccessException e) {
+                            throw new PluginException(e);
+                        }
+                    }
+                    if (converter == null) {
+                        // lookup for converter that match class simple name
+                        converter = cm.get(simpleName);
+                    }
+                }
+                converterArguments = annotation.args();
+            } else {
+                // lookup for converter that match class simple name
+                converter = cm.get(simpleName);
+                converterArguments = new Object[] {};
+            }
+            if (converter == null && PluginObjectManager.get().isBound(type)) {
+                // special converter for objects, lookup object in memory
+                converter = cm.get("object");
+                converterArguments = new Object[] { type };
+            }
+            if (converter != null) {
+                try {
+                    if (UtilLog.LOG.isDebugEnabled()) {
+                        UtilLog.LOG.debug(converter + "(" + arg + "," + Arrays.toString(converterArguments) + ")");
+                    }
+                    Object tmp = converter.convert(arg, converterArguments);
+                    if (!type.isInstance(tmp)) {
+                        throw new PluginException("Invalid parameter value [" + arg + "] in " + message + ". Expected " + type + ", received: " + tmp + " of type " + (tmp != null ? tmp.getClass() : "null"));
+                    }
+                    return tmp;
+                } catch (ConverterException e) {
+                    throw new PluginException(e);
+                }
+            }
+        }
+        return arg;
+    }
+
+    /**
+     * Extract converter information from parameter annotations.
+     * 
+     * @param annots
+     *            The annotations.
+     * @return The converter annotation if exists, null, otherwise.
+     */
+    public static Converter getConverter(Annotation[] annots) {
+        Converter conv = null;
+        for (int j = 0; j < annots.length; j++) {
+            if (annots[j] instanceof Converter) {
+                conv = (Converter) annots[j];
+            }
+        }
+        return conv;
+    }
+}
---
+++ tags/specrunner-1.3.17/specrunner-webdriver/src/main/java/org/specrunner/webdriver/actions/AbstractPluginSelect.java
@@ -0,0 +1,108 @@
+/*
+    SpecRunner - Acceptance Test Driven Development Tool
+    Copyright (C) 2011-2013  Thiago Santos
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package org.specrunner.webdriver.actions;
+
+import java.util.List;
+
+import nu.xom.Node;
+import nu.xom.Nodes;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.specrunner.SRServices;
+import org.specrunner.context.IContext;
+import org.specrunner.plugins.ActionType;
+import org.specrunner.plugins.PluginException;
+import org.specrunner.plugins.type.Command;
+import org.specrunner.result.IResultSet;
+import org.specrunner.result.IWritableFactoryManager;
+import org.specrunner.result.status.Failure;
+import org.specrunner.result.status.Success;
+import org.specrunner.webdriver.AbstractPluginFindSingle;
+
+/**
+ * Perform some action over select elements.
+ * 
+ * @author Thiago Santos
+ * 
+ */
+public abstract class AbstractPluginSelect extends AbstractPluginFindSingle {
+
+    @Override
+    public ActionType getActionType() {
+        return Command.INSTANCE;
+    }
+
+    @Override
+    protected void process(IContext context, IResultSet result, WebDriver client, WebElement element) throws PluginException {
+        if (!isSelect(element)) {
+            result.addResult(Failure.INSTANCE, context.peek(), new PluginException("Element " + getFinderInstance().resume(context) + " is not a select is " + element.getClass().getName()), SRServices.get(IWritableFactoryManager.class).get(WebDriver.class).newWritable(client));
+        } else {
+            Node node = context.getNode();
+            Nodes nodes = node.query("descendant::li");
+            if (nodes.size() == 0) {
+                nodes = new Nodes(node);
+            }
+            List<WebElement> options = element.findElements(By.xpath("descendant::option"));
+            int errors = 0;
+            for (int i = 0; i < nodes.size(); i++) {
+                Node current = nodes.get(i);
+                String option = current.getValue();
+                boolean success = false;
+                for (WebElement o : options) {
+                    if (option.equals(o.getText())) {
+                        doSomething(element, o);
+                        success = true;
+                        break;
+                    }
+                }
+                if (!success) {
+                    result.addResult(Failure.INSTANCE, context.newBlock(current, this), new PluginException("The option '" + option + "' is missing."));
+                    errors++;
+                }
+            }
+            if (errors > 0) {
+                result.addResult(Failure.INSTANCE, context.newBlock(node, this), new PluginException(errors + " option(s) is(are) missing."), SRServices.get(IWritableFactoryManager.class).get(WebDriver.class).newWritable(client));
+            } else {
+                result.addResult(Success.INSTANCE, context.newBlock(node, this));
+            }
+        }
+    }
+
+    /**
+     * Says if the element is a select.
+     * 
+     * @param element
+     *            The element.
+     * @return true, if is select, false, otherwise.
+     */
+    protected boolean isSelect(WebElement element) {
+        return element.getTagName().equalsIgnoreCase("select");
+    }
+
+    /**
+     * Perform something on select option.
+     * 
+     * @param element
+     *            The select.
+     * @param option
+     *            The option.
+     */
+    protected abstract void doSomething(WebElement element, WebElement option);
+}
