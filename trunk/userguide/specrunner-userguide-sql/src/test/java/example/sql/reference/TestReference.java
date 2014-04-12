1be4d0283b7c40fde12ea33558a5f767aae0ea92
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-04-12 02:47:32 +0000

    Bug fix on reference usage.

---
+++ trunk/userguide/specrunner-userguide-sql/src/test/java/example/sql/reference/TestReference.java
@@ -0,0 +1,34 @@
+package example.sql.reference;
+
+import org.junit.runner.RunWith;
+import org.specrunner.comparators.core.ComparatorDate;
+import org.specrunner.configuration.IConfiguration;
+import org.specrunner.junit.Configuration;
+import org.specrunner.junit.SRRunner;
+import org.specrunner.sql.IDatabase;
+import org.specrunner.sql.PluginConnection;
+import org.specrunner.sql.PluginDatabase;
+import org.specrunner.sql.PluginSchema;
+import org.specrunner.sql.PluginSchemaLoader;
+import org.specrunner.sql.impl.Database;
+import org.specrunner.sql.meta.impl.SchemaLoaderXOM;
+
+import example.sql.DataSourceProviderImpl;
+
+@RunWith(SRRunner.class)
+public class TestReference {
+
+    @Configuration
+    public void configurar(IConfiguration cfg) {
+        cfg.add(PluginConnection.FEATURE_PROVIDER_INSTANCE, new DataSourceProviderImpl());
+        cfg.add(PluginConnection.FEATURE_REUSE, true);
+        cfg.add(PluginSchemaLoader.FEATURE_PROVIDER_INSTANCE, new SchemaLoaderXOM());
+        cfg.add(PluginSchemaLoader.FEATURE_REUSE, true);
+        cfg.add(PluginSchema.FEATURE_SOURCE, "/example/sql/reference/value.cfg.xml");
+        cfg.add(PluginSchema.FEATURE_REUSE, true);
+        cfg.add(PluginDatabase.FEATURE_PROVIDER_INSTANCE, new IDatabase[] { new Database() });
+        cfg.add(PluginDatabase.FEATURE_REUSE, true);
+        cfg.add(ComparatorDate.FEATURE_TOLERANCE, 5000L);
+        // cfg.add(AbstractConverterTimezone.FEATURE_TIMEZONE, "UTC");
+    }
+}
