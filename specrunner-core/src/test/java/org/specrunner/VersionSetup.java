6476ec8b5edc2e8f44ec15aa24aa5b6ab2dc0134
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-04-25 04:47:59 +0000

    Error navigator control added.

--- specrunner-core/src/test/java/org/specrunner/VersionSetup.java
+++ specrunner-core/src/test/java/org/specrunner/VersionSetup.java
@@ -14,7 +14,7 @@ public class VersionSetup {
     public static int serial = 0;
 
     public static void main(String[] args) throws IOException {
-        adjust("", new File(System.getProperty("user.dir") + "/.."), "version>1.3.23</", "version>1.3.24</");
+        adjust("", new File(System.getProperty("user.dir") + "/.."), "version>1.3.24</", "version>1.3.25</");
     }
 
     private static void adjust(String gap, File file, String from, String to) throws IOException {
