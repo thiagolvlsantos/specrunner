7eb4ebc9673a71f3ead48c44e0e93809aa666d0b
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-05-22 13:25:55 +0000

    Pause adjusts.

--- trunk/userguide/specrunner-userguide-sql/src/test/java/example/sql/noid/TestOrdersIdentity.java
+++ trunk/userguide/specrunner-userguide-sql/src/test/java/example/sql/noid/TestOrdersIdentity.java
@@ -1,7 +1,9 @@
 package example.sql.noid;
 
 import org.junit.Test;
+import org.specrunner.sql.IDatabase;
 import org.specrunner.sql.PluginSchema;
+import org.specrunner.sql.impl.SqlDumperPrint;
 
 public class TestOrdersIdentity extends AbstractTestOrders {
 
@@ -9,6 +11,7 @@ public class TestOrdersIdentity extends AbstractTestOrders {
         super();
         cfg.add(PluginSchema.FEATURE_SOURCE, "/income/orders/orders.cfg.xml");
         cfg.add(PluginSchema.FEATURE_REUSE, true);
+        cfg.add(IDatabase.FEATURE_SQL_DUMPER, new SqlDumperPrint());
     }
 
     @Override
