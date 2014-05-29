fe16d11641ed58b7cdb22fc9fbac8a4b6d1ba589
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-05-29 13:51:41 +0000

    Version 1.3.33.

--- specrunner-core/src/test/java/org/specrunner/VersionSetup.java
+++ specrunner-core/src/test/java/org/specrunner/VersionSetup.java
@@ -14,7 +14,7 @@ public class VersionSetup {
     public static int serial = 0;
 
     public static void main(String[] args) throws IOException {
-        adjust("", new File(System.getProperty("user.dir") + "/.."), "version>1.3.31</", "version>1.3.32</");
+        adjust("", new File(System.getProperty("user.dir") + "/.."), "version>1.3.32</", "version>1.3.33</");
     }
 
     private static void adjust(String gap, File file, String from, String to) throws IOException {
