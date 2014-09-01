cd1014cff1e9e0e592a3409cd7d21b932ae28f7f
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-09-01 04:09:31 +0000

    Version 1.4.11.

--- trunk/specrunner-core/src/test/java/org/specrunner/VersionSetup.java
+++ trunk/specrunner-core/src/test/java/org/specrunner/VersionSetup.java
@@ -14,7 +14,7 @@ public class VersionSetup {
     public static int serial = 0;
 
     public static void main(String[] args) throws IOException {
-        adjust("", new File(System.getProperty("user.dir") + "/.."), "version>1.4.09</", "version>1.4.10</");
+        adjust("", new File(System.getProperty("user.dir") + "/.."), "version>1.4.10</", "version>1.4.11</");
     }
 
     private static void adjust(String gap, File file, String from, String to) throws IOException {
