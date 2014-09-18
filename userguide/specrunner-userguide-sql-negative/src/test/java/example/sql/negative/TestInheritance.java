99b812323be7ce34735d9e8de37ba6111927ce4c
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-09-18 04:07:56 +0000

    Database listeners on start/on end added.

--- userguide/specrunner-userguide-sql-negative/src/test/java/example/sql/negative/TestInheritance.java
+++ userguide/specrunner-userguide-sql-negative/src/test/java/example/sql/negative/TestInheritance.java
@@ -1,7 +1,8 @@
 package example.sql.negative;
 
 import org.specrunner.junit.SRScenarioListeners;
+import org.specrunner.sql.negative.DatabaseScenarioCompareListener;
 
-@SRScenarioListeners(inheritListeners = false)
+@SRScenarioListeners(inheritListeners = false, value = { DatabaseScenarioCompareListener.class })
 public class TestInheritance extends AbstractTest {
 }
