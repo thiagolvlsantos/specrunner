a1aeecf683ea4974005be08f24e07f8374ed616b
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-09-25 01:21:59 +0000

    Bug fix, on compare base report.

--- trunk/specrunner-core/src/main/java/org/specrunner/converters/core/ConverterEnumValueTemplate.java
+++ trunk/specrunner-core/src/main/java/org/specrunner/converters/core/ConverterEnumValueTemplate.java
@@ -31,7 +31,7 @@ public abstract class ConverterEnumValueTemplate extends ConverterEnumValue {
     /**
      * Template parameters.
      */
-    private Object[] template;
+    protected Object[] template;
 
     /**
      * Conversion of specific types.
@@ -50,5 +50,10 @@ public abstract class ConverterEnumValueTemplate extends ConverterEnumValue {
     @Override
     public Object convert(Object obj, Object[] args) throws ConverterException {
         return super.convert(obj, template);
+    }
+
+    @Override
+    public Object revert(Object obj, Object[] args) throws ConverterException {
+        return super.revert(obj, template);
     }
 }
