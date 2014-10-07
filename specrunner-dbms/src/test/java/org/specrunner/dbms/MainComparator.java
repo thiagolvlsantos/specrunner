60ba42f5e81c72af9b063eeb4892ec448b726afc
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-10-07 01:34:17 +0000

    Refactoring.

--- specrunner-dbms/src/test/java/org/specrunner/dbms/MainComparator.java
+++ specrunner-dbms/src/test/java/org/specrunner/dbms/MainComparator.java
@@ -5,6 +5,6 @@ public class MainComparator {
     public static void main(String[] args) throws Exception {
         ConnectionInfo old = new ConnectionInfo("PUBLIC.ORD", "org.hsqldb.jdbcDriver", "jdbc:hsqldb:hsql://localhost/sql", "sa", "");
         ConnectionInfo current = new ConnectionInfo("PUBLIC.DEP", "org.hsqldb.jdbcDriver", "jdbc:hsqldb:hsql://localhost/sql2", "sa", "");
-        new BaseComparator().compare(old, old, new ConfigurationFiles("/sr_dbms_tables.properties"), new ConfigurationFiles("/sr_dbms_columns.properties"));
+        new BaseComparator().compare(old, current, new ConfigurationFiles("/sr_dbms_tables.properties"), new ConfigurationFiles("/sr_dbms_columns.properties"));
     }
 }
