aa41931d41a5219a048af6498211528a36d96b3e
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2014-10-07 03:18:40 +0000

    Version 1.4.24.

--- trunk/specrunner/deploy.bat
+++ trunk/specrunner/deploy.bat
@@ -1,2 +1,2 @@
 @rem mvn clean package verify release:perform -Darguments=-Dgpg.passphrase=%1
-del /S /Q %USERPROFILE%\.m2\repository\org\specrunner & cls & mvn -U -Pdeploy clean source:jar javadoc:jar deploy & copy /Y target\*clean.zip ..\downloads & copy /Y ..\tools\specrunner-tools\target\*clean.zip ..\tools\downloads & userguide.bat
+del /S /Q %USERPROFILE%\.m2\repository\org\specrunner & cls & mvn -U -Pdeploy clean source:jar javadoc:jar deploy & copy /Y target\*clean.zip ..\downloads & copy /Y ..\tools\specrunner-tools\target\*clean.zip ..\tools\downloads & tools.bat & userguide.bat
