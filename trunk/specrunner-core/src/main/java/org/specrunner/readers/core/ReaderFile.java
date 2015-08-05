1c23dd01c0a44cc326063a477fa06382e9bdbc9b
thiago.lvl.santos <thiago.lvl.santos@41b30080-7bc5-4837-b862-23aa8836c6ba>
2015-08-05 04:17:48 +0000

    - Copyright notes updated; - Reader support extended.

--- trunk/specrunner-core/src/main/java/org/specrunner/readers/core/ReaderFile.java
+++ trunk/specrunner-core/src/main/java/org/specrunner/readers/core/ReaderFile.java
@@ -17,16 +17,28 @@
  */
 package org.specrunner.readers.core;
 
-import java.io.IOException;
+import java.io.File;
 import java.net.URI;
 
+import org.specrunner.SRServices;
+import org.specrunner.configuration.IConfiguration;
 import org.specrunner.context.IContext;
+import org.specrunner.dumper.core.ConstantsDumperFile;
+import org.specrunner.features.IFeatureManager;
+import org.specrunner.plugins.PluginException;
+import org.specrunner.plugins.core.include.IResolver;
+import org.specrunner.plugins.core.include.PluginInclude;
+import org.specrunner.plugins.core.include.ResolverDefault;
 import org.specrunner.readers.IReader;
 import org.specrunner.readers.ReaderException;
 import org.specrunner.source.ISource;
+import org.specrunner.source.ISourceFactoryManager;
+import org.specrunner.source.SourceException;
 import org.specrunner.util.UtilIO;
+import org.specrunner.util.UtilLog;
 import org.specrunner.util.xom.node.INodeHolder;
 
+import nu.xom.Attribute;
 import nu.xom.Element;
 import nu.xom.Node;
 import nu.xom.Nodes;
@@ -40,8 +52,34 @@ import nu.xom.Nodes;
 @SuppressWarnings("serial")
 public class ReaderFile implements IReader {
 
+    /**
+     * Relative URI resolver.
+     */
+    protected IResolver resolver = new ResolverDefault();
+
+    /**
+     * Resolver of relative links.
+     * 
+     * @return The current resolver.
+     */
+    public IResolver getResolver() {
+        return resolver;
+    }
+
+    /**
+     * Change the resolver.
+     * 
+     * @param resolver
+     *            A resolver.
+     */
+    public void setResolver(IResolver resolver) {
+        this.resolver = resolver;
+    }
+
     @Override
     public void initialize() {
+        IFeatureManager fm = SRServices.getFeatureManager();
+        fm.set(PluginInclude.FEATURE_RESOLVER, this);
     }
 
     @Override
@@ -56,18 +94,61 @@ public class ReaderFile implements IReader {
         StringBuilder sb = new StringBuilder();
         Nodes ns = node.query("descendant::a[@href]");
         for (int i = 0; i < ns.size(); i++) {
-            String file = ((Element) ns.get(i)).getAttribute("href").getValue();
+            Element element = (Element) ns.get(i);
+            String file = element.getAttribute("href").getValue();
             ISource source = context.getCurrentSource();
             try {
                 URI uri = source.getURI();
-                file = uri.resolve(file).getPath();
+                URI newHref = uri.resolve(file);
+                updateHref(element, newHref);
+                file = newHref.getPath();
                 file = file.substring(1);
                 sb.append(UtilIO.readFile(file));
-            } catch (IOException e) {
+            } catch (Exception e) {
                 throw new ReaderException(e);
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * Get source from a URI.
+     * 
+     * @param newHref
+     *            The URI.
+     * @return The corresponding source.
+     * @throws PluginException
+     *             On reading errors.
+     */
+    protected ISource getSource(URI newHref) throws PluginException {
+        ISource newSource = null;
+        try {
+            newSource = SRServices.get(ISourceFactoryManager.class).newSource(newHref.toString());
+        } catch (SourceException e) {
+            if (UtilLog.LOG.isDebugEnabled()) {
+                UtilLog.LOG.debug(e.getMessage(), e);
+            }
+            throw new PluginException(e);
+        }
+        return newSource;
+    }
+
+    /**
+     * Update href link of included files.
+     * 
+     * @param element
+     *            Href container.
+     * @param newHref
+     *            The href source.
+     * @throws PluginException
+     *             On calculation error.
+     */
+    protected void updateHref(Element element, URI newHref) throws PluginException {
+        IConfiguration cfg = SRServices.getFeatureManager().getConfiguration();
+        File f = new File((File) cfg.get(ConstantsDumperFile.FEATURE_OUTPUT_DIRECTORY), (String) cfg.get(ConstantsDumperFile.FEATURE_OUTPUT_NAME));
+        ISource newSource = getSource(newHref);
+        URI link = resolver.resolve(f.toURI(), newSource.getURI());
+        element.addAttribute(new Attribute("href", link.toString()));
     }
 
     @Override
