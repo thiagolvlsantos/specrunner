2d928d0f8d245d7f7d05fffec09473f57b2dbcce
Thiago Santos <thiago.lvl.santos@gmail.com>
2015-08-14 10:38:28 -0300

    Version upgrade.

--- specrunner-core/src/test/java/org/specrunner/VersionSetup.java
+++ specrunner-core/src/test/java/org/specrunner/VersionSetup.java
@@ -17,7 +17,7 @@ public class VersionSetup extends AbstractSetup {
                 return pathname.isDirectory() || pathname.getName().equalsIgnoreCase("pom.xml");
             }
         };
-        adjust("", new File(System.getProperty("user.dir") + "/.."), filter, "version>1.4.44</", "version>1.4.45</");
+        adjust("", new File(System.getProperty("user.dir") + "/.."), filter, "version>1.4.45</", "version>1.4.46</");
     }
     // CHECKSTYLE:ON
 }
