65f19181c3b55c00b752ada72d6ac893fbfcf5b1
Thiago Santos <thiago.lvl.santos@gmail.com>
2015-11-17 00:33:58 -0300

    1.5.51

--- specrunner-core/src/test/java/org/specrunner/VersionSetup.java
+++ specrunner-core/src/test/java/org/specrunner/VersionSetup.java
@@ -17,7 +17,7 @@ public class VersionSetup extends AbstractSetup {
                 return pathname.isDirectory() || pathname.getName().equalsIgnoreCase("pom.xml");
             }
         };
-        adjust("", new File(System.getProperty("user.dir") + "/.."), filter, "version>1.4.49</", "version>1.4.50</");
+        adjust("", new File(System.getProperty("user.dir") + "/.."), filter, "version>1.4.50</", "version>1.4.51</");
     }
     // CHECKSTYLE:ON
 }
