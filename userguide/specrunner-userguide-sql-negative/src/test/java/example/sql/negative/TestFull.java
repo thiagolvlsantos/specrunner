a03763042bdefa51204037c6d9790b380682fa19
Thiago Santos <thiago.lvl.santos@gmail.com>
2016-06-17 09:45:47 -0300

    Example of full error list.

--- userguide/specrunner-userguide-sql-negative/src/test/java/example/sql/negative/TestFull.java
+++ userguide/specrunner-userguide-sql-negative/src/test/java/example/sql/negative/TestFull.java
@@ -3,6 +3,7 @@ package example.sql.negative;
 import org.specrunner.configuration.IConfiguration;
 import org.specrunner.junit.Configuration;
 import org.specrunner.junit.ExpectedMessages;
+import org.specrunner.result.IResultSet;
 import org.specrunner.result.core.StringTestContainsParts;
 
 @ExpectedMessages(value = { "missing|not equals|extra" }, sorted = true, criteria = StringTestContainsParts.class)
@@ -11,6 +12,7 @@ public class TestFull extends AbstractTest {
 
     @Configuration
     public void removeFullTrace(IConfiguration cfg) {
+        cfg.add(IResultSet.FEATURE_ERROR_MAX_SIZE, null);
         // cfg.add(IResultSet.FEATURE_FULL_TRACE, false);
     }
 }
