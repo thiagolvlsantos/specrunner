f9c473241fc9da2fb62afa8fcd850dae40186ee8
Thiago Santos <thiago.lvl.santos@gmail.com>
2021-06-22 00:17:03 -0300

    Create copyTests.sh

---
+++ copyTests.sh
@@ -0,0 +1,3 @@
+ls -la
+cp -R userguide/specrunner-userguide-core/src/test/java/* specrunner-core/src/test/java/ 
+cp -R userguide/specrunner-userguide-core/src/test/resources/* specrunner-core/src/test/resources/
