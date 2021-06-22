84599934eeec3b7368efc6b3b6b4048f850c9c4b
Thiago Santos <thiago.lvl.santos@gmail.com>
2021-06-22 01:58:25 -0300

    Update copyTests.sh

--- copyTests.sh
+++ copyTests.sh
@@ -5,8 +5,14 @@ for x in core ant
 do
   echo "Copiando $x"
   export y=${x}/src/test/
+  mkdir specrunner-${x}/src
+  mkdir specrunner-${x}/src/test
+  mkdir specrunner-${x}/src/test/java
   cp -R userguide/specrunner-userguide-${y}java/* specrunner-${y}java/
   find specrunner-${y}java/
+  mkdir specrunner-${x}/src
+  mkdir specrunner-${x}/src/test
+  mkdir specrunner-${x}/src/test/resources
   cp -R userguide/specrunner-userguide-${y}resources/* specrunner-${y}resources/
   find specrunner-${y}resources/
 done;
