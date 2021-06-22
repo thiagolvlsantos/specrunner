4febb3a56f4bbbe80f634a9f1ca9eeafb71a35ba
Thiago Santos <thiago.lvl.santos@gmail.com>
2021-06-22 01:34:32 -0300

    Update copyTests.sh

--- copyTests.sh
+++ copyTests.sh
@@ -4,7 +4,7 @@ ls -la
 for x in core core-excel core-text core-spring
 do
   echo "Copiando $x"
-  export y = ${x}/src/test/
+  export y=${x}/src/test/
   cp -R userguide/specrunner-userguide-${y}java/* specrunner-${y}java/
   find specrunner-${y}java/
   cp -R userguide/specrunner-userguide-${y}resources/* specrunner-${y}resources/
